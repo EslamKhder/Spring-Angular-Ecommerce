a1d506477b79dfd5cb522a98a18a1b46237cc30e
Eslam-Khder <eslamkhder91@gmail.com>
2020-07-31 00:20:53 +0200

    disable HTTP Method for Product and ProductCategory

---
+++ backend/SpringBoot-Commerce/src/main/java/com/springboot/config/ConfigureRepositoryRestConfigration.java
@@ -0,0 +1,28 @@
+package com.springboot.config;
+
+
+import com.springboot.entity.Product;
+import com.springboot.entity.ProductCategory;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+public class ConfigureRepositoryRestConfigration implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] unsupportedaction = {HttpMethod.PUT,HttpMethod.POST,HttpMethod.DELETE};
+
+        // disable HTTP Method for Product
+        config.getExposureConfiguration()
+              .forDomainType(Product.class)
+              .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedaction))
+              .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedaction));
+
+        // disable HTTP Method for ProductCategory
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedaction))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedaction));
+
+    }
+}
