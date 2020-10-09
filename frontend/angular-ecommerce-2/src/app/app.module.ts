d088a7907743e99b7db26b695bdd4dc07995f46f
EslamKhder <eslamkhder81@gmail.com>
2020-10-09 22:36:50 +0200

    Add Routes to App-Module Angular Closes #1

--- frontend/angular-ecommerce-2/src/app/app.module.ts
+++ frontend/angular-ecommerce-2/src/app/app.module.ts
@@ -6,13 +6,22 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
+import {Router, RouterModule, Routes} from "@angular/router";
 
+const routes: Routes = [
+  {path: 'category/:id', component: ProductComponent},
+  {path: 'category', component: ProductComponent},
+  {path: 'products', component: ProductComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
